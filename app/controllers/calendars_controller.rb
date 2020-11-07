2016e720835142e6072ee7b3a77c291aa9531b13
okinashunsuke <kiseven0734@gmail.com>
2020-11-07 12:27:53 +0900

    4〜６修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -23,7 +23,7 @@ class CalendarsController < ApplicationController
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+    # 例) 今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
 
@@ -36,10 +36,12 @@ class CalendarsController < ApplicationController
       end
       wday_num = Date.today.wday #を利用して添字となる数値を得る
       #もしもwday_numが7以上であれば、7を引く
-      if wday_num >= 7
-        wday_num -= 7
+      if wday_num >= 6
+        wday_num = wday_num -6
+      else
+        wday_num = wday_num 
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num + x -1]}
       @week_days.push(days)
     end
 
